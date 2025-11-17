30bfdf8f358b18eb85f326a07034426f51eb2ac3
aymane-06 <aymane.himame@gmail.com>
2025-11-17 09:18:49 +0100

    feat: Configure LogiTrackApplicationTests to use test profile and properties for improved testing environment

--- src/test/java/com/logitrack/logitrack/LogiTrackApplicationTests.java
+++ src/test/java/com/logitrack/logitrack/LogiTrackApplicationTests.java
@@ -2,8 +2,12 @@ package com.logitrack.logitrack;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@TestPropertySource(locations = "classpath:application-test.properties")
 class LogiTrackApplicationTests {
 
     @Test
